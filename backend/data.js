ab902d2417b9eb9dd6accec755d7608717bcb793
Joe <weonmart12@gmail.com>
2020-06-06 20:41:27 -0300

    MERN Ecommerce

---
+++ backend/data.js
@@ -0,0 +1,84 @@
+export default {
+	products:[
+	{
+		_id: '1',
+		name: 'Iphone XS Max',
+		category: 'Phone',
+		image: '/images/1.jpg',
+		price: 1600,
+		brand: 'Apple',
+		rating: 4.5,
+		numReviews: 10
+	},
+	{
+		_id: '2',
+		name: 'Iphone 8 plus',
+		category: 'Phone',
+		image: '/images/2.jpg',
+		price: 850,
+		brand: 'Apple',
+		rating: 4.2,
+		numReviews: 5
+	},
+	{
+		_id: '3',
+		name: 'Macbook Air',
+		category: 'Computer',
+		image: '/images/3.jpg',
+		price: 1700,
+		brand: 'Apple',
+		rating: 4.1,
+		numReviews: 8
+	},
+	{
+		_id: '4',
+		name: 'Facebook Oculus Virtual Reality',
+		category: 'Computer',
+		image: '/images/4.jpg',
+		price: 490,
+		brand: 'Facebook',
+		rating: 4.1,
+		numReviews: 8
+	},
+	{
+		_id: '5',
+		name: 'Woofer Home Edition',
+		category: 'Computer',
+		image: '/images/5.jpg',
+		price: 60,
+		brand: 'Sony',
+		rating: 4.5,
+		numReviews: 10
+	},
+	{
+		_id: '6',
+		name: 'Samsung Galaxy S7 Edge',
+		category: 'Phone',
+		image: '/images/6.jpg',
+		price: 550,
+		brand: 'Samsung',
+		rating: 4.2,
+		numReviews: 5
+	},
+	{
+		_id: '7',
+		name: 'Huawei Mate Pro',
+		category: 'Computer',
+		image: '/images/7.jpg',
+		price: 470,
+		brand: 'Huawei',
+		rating: 4.1,
+		numReviews: 8
+	},
+	{
+		_id: '8',
+		name: 'Samsung pro',
+		category: 'Computer',
+		image: '/images/8.jpg',
+		price: 690,
+		brand: 'Samsung',
+		rating: 4.1,
+		numReviews: 8
+	}
+	]
+}
