0ba1f6f2886e987eb95710663af520d344b96ead
Joe <weonmart12@gmail.com>
2020-06-07 19:04:00 -0300

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+//const mongodbUrl =config.MONGODB_URL;	
+mongoose.connect('mongodb://localhost/amazon', { //mongodbUrl, 
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose is connected !!!');
+});
 
 const app = express();
+
+app.use("/api/users", userRoute);
 
 app.get("/api/products/:id", (req, res) => {
 	const productId = req.params.id;
